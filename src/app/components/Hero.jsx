4dad39945944898682e06cef4299e240d5d72563
Oscar <albertlund121@gmail.com>
2025-04-27 23:19:13 +0200

    igen

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -2,13 +2,13 @@ const Hero = ({}) => {
   return (
     <section className="min-h-screen flex flex-col justify-center px-12 pt-1 relative">
       <div className="mb-10 ">
-        <h1 className="text-4xl md:text-9xl font-bold break-words max-w-full mb-5">
+        <h1 className="text-6xl md:text-9xl font-bold break-words max-w-full mb-5">
           Multimedie studerende
         </h1>
-        <h1 className="text-2xl md:text-7xl font-bold leading-tight text-gray-400 mb-10">
+        <h1 className="text-4xl md:text-7xl font-bold leading-tight text-gray-400 mb-10">
           Frontend developer
         </h1>
-        <h3 className="text-lg md:text-4xl mt-4">Oscar Lund Albertsen</h3>
+        <h3 className="text-xl md:text-4xl mt-4">Oscar Lund Albertsen</h3>
       </div>
       <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2">
         <a href="#about" className="text-6xl text-gray-400">
