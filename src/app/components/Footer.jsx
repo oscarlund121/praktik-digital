4160f7595d92091cec8e8ca2416e8032e0a4889a
Oscar <albertlund121@gmail.com>
2025-04-28 00:49:35 +0200

    done

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,31 +5,24 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="w-full bg-gray-100 py-10 mt-20">
-      <div id="contact" className="py-20 px-8 mx-auto text-center">
-        <h2 className="text-2xl md:text-4xl font-bold mb-8">Kontakt</h2>
-        <p className="text-base md:text-lg mb-6">
-          Fang mig gerne på mail, mobil eller LinkedIn – jeg håber at høre fra
-          jer.
-        </p>
-        <div className="flex flex-col gap-4 items-center">
-          <a
-            href="mailto:oscar.lund@example.com"
-            className="text-lg md:text-xl text-gray-400 hover:text-black underline"
-          >
-            albertlund121@gmail.com
-          </a>
-          <a
-            href="tel:+4522233344"
-            className="text-lg md:text-xl text-gray-400 hover:text-black underline"
-          >
-            +45 22 23 25 39
-          </a>
-        </div>
-      </div>
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-8 px-6">
-        <p className="text-gray-500 text-sm text-center md:text-left">
-          © 2025 Oscar Lund Albertsen.
-        </p>
+
+        {/* Venstre side */}
+        <div className="flex flex-col items-center md:items-start text-gray-500 text-sm gap-2">
+          <p>
+            <a href="mailto:albertlund121@gmail.com" className="hover:underline">
+              albertlund121@gmail.com
+            </a>
+          </p>
+          <p>
+            <a href="mobil:+45 22 23 25 39" className="hover:underline">
+              +45 22 22 33 44
+            </a>
+          </p>
+          <p>© {new Date().getFullYear()} Oscar Lund Albertsen.</p>
+        </div>
+
+        {/* Højre side */}
         <div className="flex gap-6">
           <a
             href="https://github.com/oscarlund121"
@@ -60,6 +53,7 @@ const Footer = () => {
             />
           </a>
         </div>
+
       </div>
     </footer>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,12 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Project";
 
-
 export default function Home() {
   return (
  <div> 
 <Hero />
 <About />
 <Projects />
-
     </div>
   );
 }
