48cd0efb8130cf4bfd9a5bd50b3e1b82def81b87
Oscar <albertlund121@gmail.com>
2025-04-28 00:36:50 +0200

    done næsten

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Project";
 
+
 export default function Home() {
   return (
  <div> 
 <Hero />
 <About />
 <Projects />
+
     </div>
   );
 }
